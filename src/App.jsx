0e3eccd0d27f94673e0e3f8beb3798e689e84df6
mqkhan <qasim_ajk@yahoo.com>
2024-01-03 19:37:55 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import { Overview } from "./Overview.jsx";
+import { Flex, Text, Card, Box, Container, Table } from "@radix-ui/themes";
+export default function App() {
+  return (
+    <div>
+      <Container className="container">
+        <h1>Dashboard</h1>
+        <Flex className="card">
+          <Card style={{ maxWidth: 240 }}>
+            <Flex gap="3" align="center">
+              <Box>
+                <Text as="div" size="2" weight="bold">
+                  Teodros Girmay
+                </Text>
+                <Text as="div" size="2" color="gray">
+                  Engineering
+                </Text>
+              </Box>
+            </Flex>
+          </Card>
+          <Card style={{ maxWidth: 240 }}>
+            <Flex gap="3" align="center">
+              <Box>
+                <Text as="div" size="2" weight="bold">
+                  Teodros Girmay
+                </Text>
+                <Text as="div" size="2" color="gray">
+                  Engineering
+                </Text>
+              </Box>
+            </Flex>
+          </Card>
+          <Card style={{ maxWidth: 240 }}>
+            <Flex gap="3" align="center">
+              <Box>
+                <Text as="div" size="2" weight="bold">
+                  Teodros Girmay
+                </Text>
+                <Text as="div" size="2" color="gray">
+                  Engineering
+                </Text>
+              </Box>
+            </Flex>
+          </Card>
+          <Card style={{ maxWidth: 240 }}>
+            <Flex gap="3" align="center">
+              <Box>
+                <Text as="div" size="2" weight="bold">
+                  Teodros Girmay
+                </Text>
+                <Text as="div" size="2" color="gray">
+                  Engineering
+                </Text>
+              </Box>
+            </Flex>
+          </Card>
+        </Flex>
+        <Flex>
+          <Overview />
+          <Table.Root>
+            <Table.Header>
+              <Table.Row>
+                <Table.ColumnHeaderCell>Full name</Table.ColumnHeaderCell>
+                <Table.ColumnHeaderCell>Email</Table.ColumnHeaderCell>
+              </Table.Row>
+            </Table.Header>
+
+            <Table.Body>
+              <Table.Row>
+                <Table.RowHeaderCell>Danilo Sousa</Table.RowHeaderCell>
+                <Table.Cell>danilo@example.com</Table.Cell>
+              </Table.Row>
+
+              <Table.Row>
+                <Table.RowHeaderCell>Zahra Ambessa</Table.RowHeaderCell>
+                <Table.Cell>zahra@example.com</Table.Cell>
+              </Table.Row>
+
+              <Table.Row>
+                <Table.RowHeaderCell>Jasper Eriksson</Table.RowHeaderCell>
+                <Table.Cell>jasper@example.com</Table.Cell>
+              </Table.Row>
+            </Table.Body>
+          </Table.Root>
+        </Flex>
+      </Container>
+    </div>
+  );
+}
